46eaebd689cef1ea3825be676463487d837d830d
Forhad Ahmed <forhad@codemaster.io>
2024-10-28 23:34:11 +0600

    Add example of exclusion auto configuration.

--- SpringSession7B/src/main/java/com/codemaster/io/MainApplication.java
+++ SpringSession7B/src/main/java/com/codemaster/io/MainApplication.java
@@ -2,11 +2,15 @@ package com.codemaster.io;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.context.ApplicationContext;
 
-@SpringBootApplication()
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
 public class MainApplication {
     public static void main(String[] args) {
+        DataSourceAutoConfiguration dataSourceAutoConfiguration;
+
         ApplicationContext ctx = SpringApplication.run(MainApplication.class);
     }
 }
